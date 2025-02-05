f264f90c3c35806aa4194ffa9fc199487039db56
MiguelSperle <miguelsperledeveloper@gmail.com>
2025-02-05 11:27:26 -0300

    refactor: to replace the class type variable to a type generic more safer

--- src/main/java/com/miguelsperlee/spring_mediator_example/mediator/SpringMediator.java
+++ src/main/java/com/miguelsperlee/spring_mediator_example/mediator/SpringMediator.java
@@ -28,15 +28,20 @@ public class SpringMediator implements Mediator {
             throw new NullPointerException("The given request object cannot be null");
         }
 
-        final Class requestType = request.getClass();
+        // Types used to find the exact handler that is required to handle the given request.
+        // We are able to search all application beans(class managed by spring) by type from the Spring ApplicationContext.
+        final Class<?> requestType = request.getClass();
         final Class<OUT> responseType = (Class<OUT>) ((ParameterizedType) requestType.getGenericInterfaces()[0]).getActualTypeArguments()[0];
 
+        // Retrieve RequestHandler beans based on request and response types.
         final String[] beanNames = this.abstractApplicationContext.getBeanNamesForType(ResolvableType.forClassWithGenerics(RequestHandler.class, requestType, responseType));
 
+        // There must be a registered handler.
         if (beanNames.length == 0) {
             throw new IllegalStateException("No handlers seemed to be registered to handle the given request. Make sure the handler is defined and marked as a spring component");
         }
 
+        // There may not be more than one handler.
         if (beanNames.length > 1) {
             throw new IllegalStateException("More than one handlers found. Only one handler per request is allowed.");
         }
